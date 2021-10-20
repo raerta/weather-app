64965089f6385b55e88289b865511fdb0c557351
Ramazan Ertan <ramazanertan@gmail.com>
2021-10-20 12:21:19 +0300

    refresh icon added. some css changes

--- frontend/src/components/Weather.js
+++ frontend/src/components/Weather.js
@@ -110,7 +110,23 @@ function Weather() {
 
           {filteredCities && filteredCities.length > 0 && (
             <div className="filterContainer">
-              <button onClick={handleRefresh}> Refresh</button>
+              <button onClick={handleRefresh}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  width="15px"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
+                  />
+                </svg>{" "}
+                Refresh
+              </button>
               <input
                 onChange={(e) => setFilterText(e.target.value)}
                 type="text"
